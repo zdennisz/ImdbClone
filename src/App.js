4f2758f31866360a8657603847639306b314d766
Dennis Zabolotny <zdennisz@gmail.com>
2021-08-10 13:51:04 +0300

    refactored popup search results

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import VideoList from "./component/VideoList/VideoList";
 import SearchBar from "./component/SearchBar/SearchBar";
 import SignIn from "./component/SignIn/Signin";
 import Logo from "./component/Logo/Logo";
 export default function App() {
-  const [data, setData] = useState();
-  let resultList;
-
-  const renderDataList = (data) => {
-    setData(data);
-  };
-
-  if (data) {
-    resultList = <VideoList items={data} />;
-  }
   useEffect(() => {
     //chek the date and send request
     const date = Date();
@@ -24,10 +13,7 @@ export default function App() {
     <div className="App">
       <div className="header_cont">
         <Logo title="IMDb" />
-        <SearchBar setData={renderDataList} />
-        <div className="search_results_container">
-          {data ? resultList : null}
-        </div>
+        <SearchBar />
         <div className="vertical_line"></div>
         <SignIn />
       </div>
--- src/component/SearchBar/SearchBar.js
+++ src/component/SearchBar/SearchBar.js
@@ -1,15 +1,19 @@
 import "./SearchBar.css";
 import React, { useState } from "react";
 import SearchAccess from "../SearchAccess/SearchAcess";
+import VideoList from "../VideoList/VideoList";
 const SearchBar = (props) => {
   const [input, setInput] = useState("");
   const [btnStyle, setBtnStyle] = useState("btn btn_submit");
   const [serachCriteria, setSearchCriteria] = useState("actors");
-
+  const [data, setData] = useState();
   const inputHandler = (event) => {
     setInput(event.target.value);
   };
-
+  let resultList;
+  if (data) {
+    resultList = <VideoList items={data} />;
+  }
   const selectChangedhandler = (event) => {
     switch (event.target.value) {
       case "Titles":
@@ -38,7 +42,7 @@ const SearchBar = (props) => {
       try {
         const res = await SearchAccess(input.toString(), "auto-complete");
         setBtnStyle("btn btn_submit");
-        props.setData(res.d);
+        setData(res.d);
       } catch (err) {
         console.log(err);
       }
@@ -47,6 +51,7 @@ const SearchBar = (props) => {
   };
   return (
     <div className="search_container">
+      <div className="search_results_container">{data ? resultList : null}</div>
       <div className="select_search">
         <select className="select" onChange={selectChangedhandler}>
           <option value="All">All</option>
