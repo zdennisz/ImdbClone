20e95ee746ecb6d6623c83f8cd4e123d276dfa70
Dennis Z <zdennisz@gmail.com>
2021-08-15 15:44:04 +0300

    added styles to icons

--- src/component/LogIn/LogIn.js
+++ src/component/LogIn/LogIn.js
@@ -39,9 +39,9 @@ const LogIn = (props) => {
       <div className="login_container">
         <div className="left_col">
           <label className="sign_in_text">Sign in</label>
-          <CustomButton btnTitle="Sign in with IMDb" iconSource="fab fa-imdb fa-2x" handler={mockSignInHandler} />
-          <CustomButton btnTitle="Sign in with Amazon" iconSource="fab fa-amazon fa-2x" handler={mockSignInHandler} />
-          <CustomButton btnTitle="Sign in with Facebook" iconSource="fab fa-facebook fa-2x" handler={mockSignInHandler} />
+          <CustomButton btnTitle="Sign in with IMDb" iconSource="fab fa-imdb fa-2x imdb_logo_color" handler={mockSignInHandler} />
+          <CustomButton btnTitle="Sign in with Amazon" iconSource="fab fa-amazon fa-2x amazon_logo_color" handler={mockSignInHandler} />
+          <CustomButton btnTitle="Sign in with Facebook" iconSource="fab fa-facebook fa-2x face_logo_color" handler={mockSignInHandler} />
           <CustomButton btnTitle="Sign in with Google" iconSource="fab fa-google fa-2x" handler={googleSignInHandler} />
           <CustomButton btnTitle="Sign in with Apple" iconSource="fab fa-apple fa-2x" handler={mockSignInHandler} />
           <div className="or_segment">
